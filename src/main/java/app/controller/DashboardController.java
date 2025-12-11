2d33a7a381d0765720465b2c6e21182cdda3d370
mitetodb <65173088+mitetodb@users.noreply.github.com>
2025-12-12 01:40:24 +0200

    fix: fix dashboard statistics

--- src/main/java/app/controller/DashboardController.java
+++ src/main/java/app/controller/DashboardController.java
@@ -1,13 +1,29 @@
 package app.controller;
 
+import app.repository.CustomerRepository;
+import app.repository.ShopRepository;
+import app.repository.TestPurchaseRepository;
+import app.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class DashboardController {
 
+    private final UserService userService;
+    private final CustomerRepository customerRepository;
+    private final TestPurchaseRepository testPurchaseRepository;
+    private final ShopRepository shopRepository;
+
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        model.addAttribute("totalUsers", userService.countUsers());
+        model.addAttribute("totalCustomers", customerRepository.count());
+        model.addAttribute("totalTestPurchases", testPurchaseRepository.count());
+        model.addAttribute("totalShops", shopRepository.count());
         return "dashboard";
     }
 }
